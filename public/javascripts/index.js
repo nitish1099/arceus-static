984b20d4a94aa3934ec2161988314fdae7c4b7ed
Nitish Anand <29.nitish@gmail.com>
2021-08-05 08:56:13 +0530

    mobile version

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -5,3 +5,16 @@ jarallax(document.querySelectorAll('.js-jarallax'), {
 particlesJS.load('particles-js', './particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
+
+var toggle = document.getElementById("nav-toggle");
+toggle.onclick = function(){
+  if (toggle.classList.contains('open')) {
+    toggle.classList.remove("open");
+    var navBar = document.getElementsByClassName("nav-bar")[0];
+    navBar.classList.remove("open");
+  } else {
+    toggle.classList.add("open");
+    var navBar = document.getElementsByClassName("nav-bar")[0];
+    navBar.classList.add("open");
+  }
+};
